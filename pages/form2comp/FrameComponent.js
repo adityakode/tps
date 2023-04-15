1084372f49d1f254b40bc7a95c563717c2578d1a
Aditya Kode <aditya.kode21@pccoepune.org>
2023-04-15 20:36:13 +0530

    added form 2

---
+++ pages/form2comp/FrameComponent.js
@@ -0,0 +1,20 @@
+import "./FrameComponent.css";
+
+const FrameComponent = () => {
+  return (
+    <div className="rectangle-parent">
+      <div className="frame-child" />
+      <img
+        className="form-6-english-2-1"
+        alt=""
+        src="/form6-english2-1@2x.png"
+      />
+      <input className="relwithappl" type="text" />
+      <input className="nameoffamilymember" type="text" />
+      <input className="epic" type="text" />
+      <input className="aadharcbfor2f" type="checkbox" />
+    </div>
+  );
+};
+
+export default FrameComponent;
