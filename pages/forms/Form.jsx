76eb453709964ddd7c03315a6930fb8e0665e05a
Aditya Kode <aditya.kode21@pccoepune.org>
2023-04-15 19:39:01 +0530

    added forms

---
+++ pages/forms/Form.jsx
@@ -0,0 +1,51 @@
+import "./Form.css";
+
+
+const Form = () => {
+  return (
+    
+    <div className="form-6-englishmodifird-1-parent">
+      <div className="form-6-englishmodifird-1">
+        <img
+          className="form-6-englishmodifird-11"
+          alt=""
+          src="/form6-englishmodifird-1@2x.png"
+        />
+        <input className="address" type="text" />
+      </div>
+      <input className="aadharno" />
+      <input className="first-middlename" />
+      <input className="mobself" />
+      <input className="namesurnamerelative" />
+      <input className="surname" />
+      <input className="fmdsurname" />
+      <input className="relativemob" />
+      <input className="female" type="checkbox" />
+      <input className="dob" />
+      <input className="aadharcheckbox" type="checkbox" />
+      <img className="photo-icon" alt="" src="/photo@2x.png" />
+      <input className="mother" type="checkbox" />
+      <input className="father" type="checkbox" />
+      <input className="aadharcheckbox1" type="checkbox" />
+      <input className="firstnamefollowedbymiddlename" type="text" />
+      <input className="first-name" />
+      <input className="male" type="checkbox" />
+     
+      <img
+        className="form-6-english-2-1"
+        alt=""
+        src="/form6-english2-1@2x.png"
+      />
+      <input className="adharcb2nd" type="checkbox" />
+      <input className="noffamilymember2" type="text" />
+      <input className="relwithapplicant2" type="text" />
+      <input className="epic2" type="text" />
+      <input className="cbdec" type="checkbox"/>
+      
+      <button className="bt">fgfg</button>
+    </div>
+    
+  );
+};
+
+export default Form;
